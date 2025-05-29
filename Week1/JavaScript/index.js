a34ebebda17496b18661c57b853c9d9515e3ed79
Ishan Saini <ishansaini0105@gmail.com>
2025-05-29 11:13:27 +0530

    JS intro

---
+++ Week1/JavaScript/index.js
@@ -0,0 +1,59 @@
+// the browser can only understand the HTML,CSS,Javscript and webassembly...
+// React and Nextjsd alsod elas with them only and application like google meet use web assembly...
+
+//  js is a n interpreted language which makes t slow to execute unlike cpp..
+// more vulnurable to run time errors is a huge downfall.(wihtout ts)
+// Dynamically typed.
+
+// Single threaded environment but handles it by being asynchronus...
+// only a single cpu is occupied even at the most expensive js code
+
+// in RUST or GOlang will take more then 100% uses.
+// JS uses an garbage collector for memory management.
+// nopt good for robust application slike banking etc.
+
+// let x = "Ishan";
+// console.log(x);
+// console.log(x, "is the best");
+
+// let i = 0;
+// console.log(i);
+// console.log(1);
+// console.log(2);
+
+// const isname = () => {
+//   console.log("Naruto");
+// };
+// isname();
+
+// let users = ["A", "B"];
+
+// for (let i = 0; i < users.length; i++) {
+//   console.log(users[i]);
+// }
+
+// function greet(name) {
+//   console.log("Hello", name);
+// }
+// greet("Ishan");
+
+// let x = 1;
+// while (x <= 10) {
+//   console.log("Hello", x);
+//   x++;
+// }
+
+// const Ishan = {
+//   name: "ishan",
+//   age: "20",
+//   intrest: "CS",
+// };
+
+// console.log(Ishan);
+// console.log(Ishan.name);
+
+// console.log(
+//   `Hello myself ${Ishan.name}, my age is ${Ishan.age} and I love ${Ishan.intrest}`
+// );
+
+//
