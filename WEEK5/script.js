c82c05669ae95041d2d8f4d61486a925ee565496
Ishan Saini <ishansaini0105@gmail.com>
2025-06-03 21:08:04 +0530

    Maps and filters revised

---
+++ WEEK5/script.js
@@ -0,0 +1,14 @@
+// Maps revision
+const arr =['1','2','3'];
+
+let ans = arr.map((id)=>{
+	return id * 2;
+})
+console.log(ans);
+
+
+//filters
+let ans2 = arr.filter((id)=>{
+return id == "3";
+})
+console.log(ans2)
