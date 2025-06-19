51063a33df10385c3d64c3c1ebad3aa94d80cca6
Ishan Saini <ishansaini0105@gmail.com>
2025-06-19 21:27:09 +0530

    The backedn for chat app is build without rooms

--- WEEK16/ChatApp/BackEnd/dist/app.js
+++ WEEK16/ChatApp/BackEnd/dist/app.js
@@ -1,9 +1,27 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
-const server = new ws_1.WebSocketServer({ port: 8080 });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const Port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
+const server = new ws_1.WebSocketServer({ port: Port });
 let userCount = 0;
+let allSocket = [];
 server.on("connection", (socket) => {
     userCount++;
+    allSocket.push(socket);
     console.log("user Connected : ", userCount);
+    socket.on("message", (event) => {
+        console.log("User sent a message : ", event.toString());
+        for (let i = 0; i < allSocket.length; i++) {
+            if (allSocket[i] !== socket) {
+                setTimeout(() => {
+                    allSocket[i].send(event.toString());
+                }, 1000);
+            }
+        }
+    });
 });
--- WEEK16/ChatApp/BackEnd/src/app.ts
+++ WEEK16/ChatApp/BackEnd/src/app.ts
@@ -1,10 +1,25 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from "ws";
+import dotenv from "dotenv";
+dotenv.config();
 
-const server = new WebSocketServer({ port: 8080 });
-
+const Port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
+const server = new WebSocketServer({ port: Port });
 
 let userCount = 0;
-server.on("connection",(socket)=>{
-userCount++;
-console.log("user Connected : ", userCount);
-})
+let allSocket: WebSocket[] = [];
+server.on("connection", (socket) => {
+  userCount++;
+  allSocket.push(socket);
+  console.log("user Connected : ", userCount);
+
+  socket.on("message", (event) => {
+    console.log("User sent a message : ", event.toString());
+    for (let i = 0; i < allSocket.length; i++) {
+      if (allSocket[i] !== socket) {
+        setTimeout(() => {
+          allSocket[i].send(event.toString());
+        }, 1000);
+      }
+    }
+  });
+});
